8b63e1401d831c08b07ead6a2fc867353eabb13e
sarumani <qkre2018@gmail.com>
2024-01-22 21:21:41 +0900

    페이징 기능 구현

--- src/components/mainPage.js
+++ src/components/mainPage.js
@@ -1,111 +1,127 @@
-import "./body.css";
-import React, { useEffect, useState } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
+  faArrowDown,
   faArrowTrendUp,
   faClock,
-  faArrowDown,
   faEllipsisVertical,
 } from "@fortawesome/free-solid-svg-icons";
-import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import axios from "axios";
 import moment from "moment";
 import "moment/locale/ko";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import Header from "./header";
+import "./mainPage.css";
 
 export default function MainPage(props) {
-  const { isChanged } = props;
+  const { isLogin, setIsLogin, loginModalShow, setLoginModalShow } = props;
+  const [count, setCount] = useState(0);
   const [orderBy, setOrderBy] = useState("createdAt");
-  const [posts, setPosts] = useState([]);
   const [postElement, setPostElement] = useState([]);
-  const [articles, setArticles] = useState();
-  const [articleElement, setArticleElement] = useState();
+
+  let timeout;
+
+  window.addEventListener("scroll", () => {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => {
+      const scrollable =
+        document.documentElement.scrollHeight - window.innerHeight - 100;
+      const scrolled = window.scrollY;
+      console.log(scrolled);
+      if (scrolled > scrollable) {
+        // 화면의 맨 끝까지 스크롤됨
+        console.log("맨 끝까지 스크롤됨");
+        setCount(count + 1);
+      }
+    }, 200); // 200ms 딜레이
+  });
 
   useEffect(() => {
+    console.log("API 요청 ");
     axios
       .get(`/api/public/post/all`, {
         params: {
           orderBy: orderBy,
+          page: count,
         },
       })
       .then((res) => {
-        setPosts(res.data);
-        setArticles(res.data);
-        console.log(res);
+        makePostElement(res.data);
       })
       .catch((err) => console.log(err));
-  }, []);
+  }, [count, orderBy]);
 
-  useEffect(() => {
-    try {
-      const postElementList = posts.map((post) => {
-        const contentElement = new DOMParser()
-          .parseFromString(post.content, "text/html")
-          .querySelector("div");
+  const makePostElement = (postDatas) => {
+    console.log(postDatas);
 
-        var thumbnailJSX;
+    const newPostElement = postDatas.map((post) => {
+      const key = post.postId;
+      const contentElement = new DOMParser()
+        .parseFromString(post.content, "text/html")
+        .querySelector("div");
 
-        try {
-          const thumbnail = contentElement.querySelector("img");
+      var thumbnailJSX;
 
-          thumbnailJSX = React.createElement("div", {
-            dangerouslySetInnerHTML: {
-              __html: thumbnail.outerHTML,
-            },
-          });
-        } catch (err) {
-          thumbnailJSX = React.createElement("div");
-        }
+      try {
+        const thumbnail = contentElement.querySelector("img");
 
-        let firstText;
-        try {
-          const childNodes = Array.from(contentElement.childNodes);
-          const firstDIV = childNodes.find((item) => {
-            if (item.tagName === "DIV" && item.innerText !== "") {
-              return item;
-            }
-          });
-          firstText = firstDIV.innerText;
-        } catch (err) {
-          console.log(err);
-          firstText = "내용이 없습니다.";
-        }
-        const fromNow = (createdAt) => {
-          const momentAt = moment(createdAt);
+        thumbnailJSX = React.createElement("div", {
+          dangerouslySetInnerHTML: {
+            __html: thumbnail.outerHTML,
+          },
+        });
+      } catch (err) {
+        thumbnailJSX = React.createElement("div");
+      }
 
-          return <span className="createdAt">{momentAt.fromNow()}</span>;
-        };
+      let firstText;
+      try {
+        const childNodes = Array.from(contentElement.childNodes);
+        const firstDIV = childNodes.find((item) => {
+          if (item.tagName === "DIV" && item.innerText !== "") {
+            return item;
+          }
+        });
+        firstText = firstDIV.innerText;
+      } catch (err) {
+        console.log(err);
+        firstText = "내용이 없습니다.";
+      }
+      const fromNow = (createdAt) => {
+        const momentAt = moment(createdAt);
 
-        return (
-          <Link
-            key={post.postId}
-            to={`/post/${post.user.userNickName}/${post.postId}`}
-            className="postBox"
-          >
-            <div className="innerBox">
-              <div className="thumbnail">{thumbnailJSX}</div>
-              <span className="title">{post.title}</span>
-              <span className="content">{firstText}</span>
+        return <span className="createdAt">{momentAt.fromNow()}</span>;
+      };
+
+      return (
+        <Link
+          key={key}
+          to={`/post/${post.user.userNickName}/${post.postId}`}
+          className="postBox"
+        >
+          <div className="innerBox">
+            <div className="thumbnail">{thumbnailJSX}</div>
+            <span className="title">{post.title}</span>
+            <span className="content">{firstText}</span>
+          </div>
+          <span className="date">{fromNow(post.createdAt)}</span>
+          <section className="infoSection">
+            <div>
+              <div
+                className="userIcon"
+                style={{
+                  backgroundColor: post.user.userIcon,
+                }}
+              />
+              <span className="userID">by {post.user.userNickName}</span>
             </div>
-            <span className="date">{fromNow(post.createdAt)}</span>
-            <section className="infoSection">
-              <div>
-                <div
-                  className="userIcon"
-                  style={{
-                    backgroundColor: post.user.userIcon,
-                  }}
-                />
-                <span className="userID">by {post.user.userNickName}</span>
-              </div>
-            </section>
-          </Link>
-        );
-      });
-      setPostElement(postElementList);
-    } catch (err) {
-      console.error(err);
-    }
-  }, [posts]);
+          </section>
+        </Link>
+      );
+    });
+
+    setPostElement([...postElement, ...newPostElement]);
+  };
 
   const changeOrderBy = (e) => {
     console.log(e.target.className);
@@ -113,9 +129,15 @@ export default function MainPage(props) {
   };
 
   return (
-    <div>
-      <section className={"bodyContainer"}>
-        <section className={"header"}>
+    <div className="mainPage">
+      <section className="header">
+        <Header
+          isLogin={isLogin}
+          setIsLogin={setIsLogin}
+          loginModalShow={loginModalShow}
+          setLoginModalShow={setLoginModalShow}
+        />
+        <section className={"function"}>
           <div className={"tags"}>
             <button className="likes" onClick={changeOrderBy}>
               <FontAwesomeIcon icon={faArrowTrendUp} />
@@ -133,8 +155,8 @@ export default function MainPage(props) {
             <FontAwesomeIcon icon={faEllipsisVertical} />
           </button>
         </section>
-        <section className={"body"}>{postElement}</section>
       </section>
+      <section className={"body"}>{postElement}</section>
     </div>
   );
 }
